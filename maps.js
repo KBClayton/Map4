b94604d35eb7615fec8dfaffbedc2094adc3d30e
KBClayton <clayton.kevinb@gmail.com>
2019-11-09 10:34:49 -0600

    some stuff

---
+++ maps.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      
+    async function wait() {
+        await sleep(1000);
+    }
+      
+    wait().then(function(){
+
+                var location_001={lat: 35.520387, lng: -97.624925}
+
+                var point_001 = new google.maps.Marker({
+                    position: location_001,
+                    title: "Discharge Location"
+                });
+                point_001.setMap(map); 
+                    
+    
+    })
+});
